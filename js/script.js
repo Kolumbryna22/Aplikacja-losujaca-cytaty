c96967ab27a4b89164363f6065aa69a9bdc5f68f
Dominik Wilusz <wilusz1@o2.pl>
2018-08-23 12:05:51 +0200

    Fix some issues

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 $(function() {
     'use strict';
 
-    var prefix = "https://cors-anywhere.herokuapp.com/";
     var tweetLink = "https://twitter.com/intent/tweet?text=";
     var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+    var $quote = $('.quote');
+    var $author = $('.author');
+    var $tweet = $('.tweet')
 
     function getQuote() {
+        var prefix = "https://cors-anywhere.herokuapp.com/";
+
         $.getJSON(prefix + quoteUrl, createTweet);
         $.ajaxSetup({ cache: false });
     };
@@ -15,6 +19,7 @@ $(function() {
         var quoteText = $(data.content).text().trim();
         var quoteAuthor = data.title;
         var tweetText;
+        var tweet;
 
         if (!quoteAuthor.length) {
             quoteAuthor = "Unknown author";
@@ -26,11 +31,10 @@ $(function() {
             getQuote();
         }
         else {
-            var tweet = tweetLink + encodeURIComponent(tweetText);
-
-            $('.quote').text(quoteText);
-            $('.author').text("Autor: " + quoteAuthor);
-            $('.tweet').attr('href', tweet);
+            tweet = tweetLink + encodeURIComponent(tweetText);
+            $quote.text(quoteText);
+            $author.text("Autor: " + quoteAuthor);
+            $tweet.attr('href', tweet);
         }
     };
 
@@ -40,4 +44,4 @@ $(function() {
             getQuote();
         });
     });
-})();
+});
